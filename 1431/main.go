4b57ededd00ea4b17e304eb065fb55f1fccd52dc
Nikolay Kozlov <gremiha3@gmail.com>
2024-01-04 14:09:00 +0300

    1431 leetcod

--- 1431/main.go
+++ 1431/main.go
@@ -3,17 +3,25 @@ package main
 import "fmt"
 
 func kidsWithCandies(candies []int, extraCandies int) []bool {
-	var out = []bool{
-		true,
-		true,
-		false,
+	var out []bool
+	var maxval int = 0
+	for _, val := range candies {
+		if val >= maxval {
+			maxval = val
+		}
 	}
-
+	for _, val := range candies {
+		if val+extraCandies < maxval {
+			out = append(out, false)
+		} else {
+			out = append(out, true)
+		}
+	}
 	return out
 }
 func main() {
-	c := []int{2, 3, 5, 1, 3}
-	ec := 3
+	c := []int{12, 1, 12}
+	var ec int = 10
 	fmt.Printf("candies = %v, extraCandies = %d\n", c, ec)
-	fmt.Printf("array = %v", kidsWithCandies(c, ec))
+	fmt.Printf("array = %v\n", kidsWithCandies(c, ec))
 }
